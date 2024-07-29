3af004203177e0f26b54bfae7cdc0589c754ba43
Yifan Zhou <yifan.zhou11882-bigz@basischina.com>
2024-07-29 11:32:37 +0800

    Update test_mitigation.py

--- tests/test_mitigation.py
+++ tests/test_mitigation.py
@@ -1,6 +1,5 @@
 import pytest
-from error.mitigation import your_function_to_test
+from error import get_A
 
-def test_your_function():
-  assert your_function_to_test() == expected_result
+print (get_A(4,3))
 
